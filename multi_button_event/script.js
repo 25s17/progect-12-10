b1cac292eb8ff871917d6b83e545a2330912fdda
25s17 <arunalu@chuo-ai.jp>
2025-12-10 10:50:53 +0900

    Add yellow button event listener and count

--- multi_button_event/script.js
+++ multi_button_event/script.js
@@ -1,12 +1,22 @@
 let redCount = 0;
 let blueCount = 0;
+let yellowCount = 0; // NEW COUNT VARIABLE
+
 document.getElementById("redBtn").addEventListener("click", () => {
-redCount++;
-document.getElementById("message").innerText =
-"赤ボタンを " + redCount + " 回押しました";
+    redCount++;
+    document.getElementById("message").innerText =
+        "赤ボタンを " + redCount + " 回押しました";
 });
+
 document.getElementById("blueBtn").addEventListener("click", () => {
-blueCount++;
-document.getElementById("message").innerText =
-"青ボタンを " + blueCount + " 回押しました";
+    blueCount++;
+    document.getElementById("message").innerText =
+        "青ボタンを " + blueCount + " 回押しました";
+});
+
+// NEW YELLOW BUTTON LISTENER ADDED HERE
+document.getElementById("yellowBtn").addEventListener("click", () => {
+    yellowCount++;
+    document.getElementById("message").innerText =
+        "黄ボタンを " + yellowCount + " 回押しました";
 });
